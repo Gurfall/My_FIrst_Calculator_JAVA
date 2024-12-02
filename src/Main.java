eb3f239c086588189188d0af5cee7e649781b117
Gurfall <thunderbird13245@gmail.com>
2024-12-02 20:48:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static String calc(String input) throws IOException
+    {
+        int result=0;
+        String[] userInput;
+        userInput = input.split(" ");
+        if(userInput.length>3)
+        {
+            throw new IOException();
+        }
+        int num1 = Integer.decode(userInput[0]);
+        int num2 = Integer.decode(userInput[2]);
+
+        if(num1>10 || num2 >10 || num1 < 0 || num2 <0 )
+        {
+            throw new IOException();
+        }
+
+        char operation = userInput[1].charAt(0);
+        switch (operation) {
+            case ('+'):
+                result = num1+num2;
+                break;
+
+            case ('-'):
+                result = num1-num2;
+                break;
+
+            case ('/'):
+                result = num1/num2;
+                break;
+
+            case ('*'):
+                result = num1*num2;
+                break;
+
+            default:
+                throw new IOException();
+
+        }
+
+        return String.valueOf(result);
+    }
+
+     public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        String scan = scanner.nextLine();
+        try {
+
+            System.out.println("Result : "+calc(scan));
+        }
+        catch (IOException a) {
+            System.out.println("Ошибка Ввода (Не соответствует начальным условиям)");
+        }
+    }
+}
